8675bb412efa0c7a9687f34a5d86492b27d2ee12
RiceDaddy321 <0carlitos2@gmail.com>
2023-04-10 21:23:13 -0500

    completed the code

--- hw3-1.py
+++ hw3-1.py
@@ -4,10 +4,9 @@ from keras.utils import pad_sequences, to_categorical
 import numpy as np
 import pandas as pd
 from keras.preprocessing.text import Tokenizer
+from sklearn.model_selection import train_test_split
 
-# from keras.datasets import imdb
-# (x_train, y_train), (x_test, y_test) = imdb.load_data(num_words=1000)
-f = open('Video_Games.json')
+f = open("Video_Games.json")
 
 df = pd.read_json(f, lines=True)
 df = df[['overall', 'reviewText']]
@@ -15,18 +14,17 @@ df = df.dropna()
 df = df.reset_index(drop=True)
 
 df = np.array(df)
-df = df[0:10]
-# preprocess our data
-import string
-import nltk
-from nltk.corpus import stopwords
-from nltk.stem import PorterStemmer
-import numpy as np
 
-from sklearn.model_selection import train_test_split
+f = open("Video_Games.json")
 
-# split our data
-(x_train, x_test, y_train, y_test) = train_test_split(df[:, 0], df[:, 1], test_size=0.2, random_state=42)
+df = pd.read_json(f, lines=True)
+df = df[['overall', 'reviewText']]
+df = df.dropna()
+df = df.reset_index(drop=True)
+
+df = np.array(df)
+
+(y_train, y_test, x_train, x_test) = train_test_split(df[:, 0], df[:, 1], test_size=0.2, random_state=42)
 
 # Tokenize our stuff
 tokenizer = Tokenizer(num_words=5000)
@@ -50,7 +48,7 @@ model.add(Dropout(0.5))
 model.add(Conv1D(64, 5, padding='valid', activation='relu', strides=1))
 model.add(MaxPool1D(pool_size=4))
 model.add(LSTM(55))
-model.add(Dense(5))
+model.add(Dense(6))
 
 model.summary()
 
